21fafd5f8eddf4a25beada84632e4f9727cf3817
Iyanu Afolabi <iafolabi@providusbank.com>
2023-08-09 08:56:44 +0100

    version 1

---
+++ Cousera practice/Javascript/jspractice.js
@@ -0,0 +1,138 @@
+//else if - Are you old enough?
+
+// var age=10
+// if(age >= 65){
+//     console.log('You get your income from your pension')
+// } else if ( age<65 && age>=18) {
+//     console.log('Each month you get get salary')
+// }else if( age<18) {
+//     console.log('You get an allowance')
+// }else {
+//     console.log('The value of the age variable is not numerical')
+// }
+    
+// -----------
+
+// //a switch - Code the days of the week!
+
+// var day='Sunday'
+
+// switch(day) {
+//     case 'Monday':
+//         console.log("Excercise")
+//         break
+//     case 'Tuesday':
+//         console.log("Play")
+//         break
+//     case 'Wednesday':
+//         console.log("Fast and pray")
+//         break
+//     case 'Thursday':
+//         console.log("Travel to school")
+//         break
+//     case 'Friday':
+//         console.log("Meet the lecturers")
+        
+//     case 'Saturday':
+//         console.log("Relax")
+//         break
+//     case 'Sunday':
+//         console.log("Go to church")
+//         break
+//     default :
+//         console.log("There is no such day")
+// }
+    
+// --------
+
+// for(var i=5; i<= 10; i++){
+//     console.log(i)
+// }
+// console.log(i, 'is the result',)
+
+// for(var i = 5; i>0; i--){
+//     console.log(i)
+// };
+// console.log('Counting completed!')
+
+// var i=1;
+
+// while(i<6){
+//     console.log(i);
+//     i++;
+// };
+// console.log('Counting completed!');
+
+// var cubes = 'ABCDEFG';
+// //styling console output using CSS with a %c format specifier
+// for (var i = 0; i < cubes.length; i++) {
+//     var styles = "font-size: 40px; border-radius: 10px; border: 1px solid blue; background: pink; color: purple";
+//     console.log("%c" + cubes[i], styles)
+// }
+
+
+// for (var i=1; i<=10; i++){
+//     if (i===1) {
+//         console.log("Gold medal")
+//     } else if (i===2){
+//         console.log("silver medal")
+//     }else if (i===3){
+//         console.log("Bronze medal")
+//     } else {
+//         console.log (i)
+//     }
+// }
+
+
+// for (var i=1; i<=10; i++) {
+
+// switch(i){
+//     case 1:
+//         console.log ("Gold medal")
+//         break
+//     case 2:
+//         console.log("silver medal")
+//         break
+//     case 3:
+//         console.log("Bronze medal")
+//         break
+//     default :
+//         console.log(i)
+// }
+// }
+
+
+// var i=1;
+
+// if(i == 0 && i == 1) {
+//     console.log("Hello");
+//   } else {
+//     console.log("Goodbye");
+//   }
+
+//   for (i = 0; i < 2; i++) {
+//     console.log("Hello");
+// }
+
+var i = 0;
+while(i < 3) {
+  console.log("Hello");
+  i++;
+}
+
+
+i= 7;
+if(i <= 5) {
+    console.log("Hello");
+  } else if(i <= 10) {
+    console.log("Goodnight");
+  } else {
+    console.log("Goodbye");
+  }
+
+i=3;
+if(i == 2 || i == 3) {
+    console.log("Hello");
+  } else {
+    console.log("Goodbye");
+  }
